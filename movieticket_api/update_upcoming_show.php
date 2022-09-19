c8b848cce0b71d051782dae28a61afb1f06a325c
Mitvirani310 <86394017+Mitvirani310@users.noreply.github.com>
2022-09-19 14:36:45 +0530

    Add files via upload

---
+++ movieticket_api/update_upcoming_show.php
@@ -0,0 +1,99 @@
+<?php
+
+include("dbconnection.php");
+$con = dbconnection();
+
+if(isset($_POST["moviename"]))
+{
+    $moviename = $_POST["moviename"];
+}
+else 
+{
+    return;
+}
+
+if(isset($_POST["screenno"]))
+{
+    $screenno = $_POST["screenno"];
+}
+else 
+{
+    return;
+}
+
+if(isset($_POST["totalseats"]))
+{
+    $totalseats = $_POST["totalseats"];
+}
+else 
+{
+    return;
+}
+
+if(isset($_POST["bookedseats"]))
+{
+    $bookedseats = $_POST["bookedseats"];
+}
+else 
+{
+    return;
+}
+if(isset($_POST["price"]))
+{
+    $price = $_POST["price"];
+}
+else 
+{
+    return;
+}
+
+if(isset($_POST["timing"]))
+{
+    $timing = $_POST["timing"];
+}
+else 
+{
+    return;
+}
+
+if(isset($_POST["date"]))
+{
+    $date = $_POST["date"];
+}
+else 
+{
+    return;
+}
+
+if(isset($_POST["moviecaption"]))
+{
+    $moviecaption = $_POST["moviecaption"];
+}
+else 
+{
+    return;
+}
+if(isset($_POST["movieimage"]))
+{
+    $movieimage = $_POST["movieimage"];
+}
+else 
+{
+    return;
+}
+
+$query = "UPDATE `upcoming_shows` SET `screenno`= '$screenno',`total_seats`= '$total_seats',`booked_seats`= '$booked_seats',`price`= '$price',`timing`='$timing',`date`='$data',`movie_caption`='$movie_caption',`movie_image`='movie_image', WHERE `moviename`='$moviename'";
+$exe = mysqli_query($con,$query);
+$arr = [];
+
+if($exe)
+{
+    $arr["success"] = "true";
+}
+else
+{
+    $arr["success"] = "false";
+}
+print(json_encode($arr));
+
+?>
